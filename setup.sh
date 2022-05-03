0282f72e52f80ba3c083bca0e442329ac64e4fac
alex <BieniekAlexander@gmail.com>
2022-05-03 13:37:55 -0500

    updating setup, adding output directory to vertexai job in cloudbuild

--- setup.sh
+++ setup.sh
@@ -12,15 +12,3 @@ if [ -z "$(conda env list | grep $CONDA_ENV_NAME)" ] ; then # create conda env i
 else
 	conda activate $CONDA_ENV_NAME
 fi
-
-
-echo "#######################"
-echo "### Collecting data ###"
-echo "#######################"
-# TODO
-mkdir -p data
-# gsutil -m cp \
-#	"gs://amazing-public-data/census_income/census_income_data_adult.data" \
-#	"gs://amazing-public-data/census_income/census_income_data_adult.names" \
-#	"gs://amazing-public-data/census_income/census_income_data_adult.test" \
-#	data
