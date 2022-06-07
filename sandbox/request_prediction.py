a08420bceaf9a92edda77e0201ae3d94d9181a9b
alex <BieniekAlexander@gmail.com>
2022-06-07 13:58:31 -0500

    updated request scripts

--- sandbox/request_prediction.py
+++ sandbox/request_prediction.py
@@ -1,30 +1,53 @@
 # source: https://cloud.google.com/vertex-ai/docs/predictions/online-predictions-custom-models#online_predict_custom_trained-python
 # imports
 from google.cloud import aiplatform
+import argparse
+import json
 
 # constants
-PROJECT_ID = "884338362548"
-ENDPOINT_ID = "1673755764637827072"
-REGION = "us-east1"
+SCHEMA = ['age',
+	'workclass',
+	'fnlwgt',
+	'education',
+	'education-num',
+	'marital-status',
+	'occupation',
+	'relationship',
+	'race',
+	'sex',
+	'capital-gain',
+	'capital-loss',
+	'hours-per-week',
+	'native-country']
 
-input = {
-	'age': 25,
-	'workclass': 'Private',
-	'fnlwgt': 226802,
-	'education': '11th',
-	'education-num': 7,
-	'marital-status': 'Never-married',
-	'occupation': 'Machine-op-inspct',
-	'relationship': 'Own-child',
-	'race': 'Black',
-	'sex': 'Male',
-	'capital-gain': 0,
-	'capital-loss': 0,
-	'hours-per-week': 40,
-	'native-country': 'United-States'}
 
-aiplatform.init(project=PROJECT_ID, location=REGION)
-endpoint = aiplatform.Endpoint(ENDPOINT_ID)
-prediction = endpoint.predict(instances=[list(input.values())])
+def request_prediction(project_id, endpoint_id, region, request_json):
+	"""Get a prediction from vertexAI
 
-print(prediction)
+	Args:
+			project_id
+			endpoint_id
+			region
+			json
+	"""
+	request_input = [request_json[k] for k in SCHEMA]
+	aiplatform.init(project=project_id, location=region)
+	endpoint = aiplatform.Endpoint(endpoint_id)
+	prediction = endpoint.predict(instances=[request_input])
+	print(prediction)
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Request a prediction from a model in VertexAI')
+	parser.add_argument('--project-id', dest='project_id', required=True,
+                    help='project containing the VertexAI endpoint')
+	parser.add_argument('--endpoint-id', dest='endpoint_id', required=True,
+                    help='id of the endpoint we want to hit')
+	parser.add_argument('--region', dest='region', default='us-east1',
+                    help='region where the endpoint lives')
+	parser.add_argument('--json-path', dest='json_path', default='test_sample.json',
+                    help='path where our request JSON lives')
+
+	args = parser.parse_args()
+	request_json = json.load(open(args.json_path, "r"))
+	request_prediction(args.project_id, args.endpoint_id, args.region, request_json)
--- sandbox/request_prediction.sh
+++ sandbox/request_prediction.sh
@@ -1,10 +1,34 @@
-PROJECT_ID=mw-ds-model-promotion-poc-prod
-ENDPOINT_ID=6467556092997009408
+PROJECT_ID=mw-ds-model-promotion-poc-dev
+ENDPOINT_ID=4716218787903307776
 REGION=us-east1
 INPUT_DATA_FILE=test_sample.json
+
+INSTANCES=$(python  <<EOF
+import json
+
+SCHEMA = ['age',
+	'workclass',
+	'fnlwgt',
+	'education',
+	'education-num',
+	'marital-status',
+	'occupation',
+	'relationship',
+	'race',
+	'sex',
+	'capital-gain',
+	'capital-loss',
+	'hours-per-week',
+	'native-country']
+
+request_json = json.load(open('$INPUT_DATA_FILE', "r"))
+request_input = [request_json[k] for k in SCHEMA]
+print({"instances": [request_input]})
+EOF
+)
 
 curl -X POST \
 	-H "Authorization: Bearer $(gcloud auth print-access-token)" \
 	-H "Content-Type: application/json; charset=utf-8" \
-	-d "@${INPUT_DATA_FILE}" \
+	-d "$INSTANCES" \
 	https://${REGION}-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/${REGION}/endpoints/${ENDPOINT_ID}:predict
